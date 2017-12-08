371b1c930ff62a694a895e87b3b611bc1bcb40dc
Unknown <jwregme@gmail.com>
2017-12-08 19:53:16 +0100

    add service for returning download data, testdata generation

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from flask import Flask, request, send_from_directory
+import sys
+import json
+from flask import Flask, request, send_from_directory, jsonify
+
 app = Flask(__name__)
+
+app_downloads = [] # stores the app download information (to be moved to a database)
 
 @app.route('/<path:path>', methods=['GET'])
 def getStaticFile(path):
@@ -9,5 +14,63 @@ def getStaticFile(path):
 def getIndexFile():
     return send_from_directory('resources', 'index.html')
 
+# receive new app download data
+@app.route('/service/adddownloads', methods=['POST'])
+def addDownloads():
+    data = request.json
+    app_downloads.extend(data)
+    return "", 201
+
+# return all app downloads
+@app.route('/service/getdownloads', methods=['GET'])
+def getDownloads():
+    return jsonify(app_downloads)
+
+# testdata generation
+countries = ["Spain", "Italy", "Germany", "Switzerland", "Australia", "Japan", "Singapore", "US", "Argentina", "Chile", "Colombia"]
+appids = ["IOS_ALERT", "IOS_MATE", "ANDROID_ALERT", "ANDROID_MATE"]
+def generateTestdata():
+    ret = {}
+    ret["country"] = countries[randint(0, len(countries) - 1)]
+    ret["app_id"] = appids[randint(0, len(appids) - 1)]
+    d = datetime.today() - timedelta(seconds=randint(0, 60*60*24*3*356)) # three years back until now
+    ret["downloaded_at"] = d.isoformat(' ')
+    ret["longitude"] = -180.0 + randint(0, 360000)/1000.0
+    ret["latitude"] = -90.0 + randint(0, 180000)/1000.0
+    return ret
+     
+
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    usage = """either provide no arguments to start the server or specify 'add-testdata' followed by an integer to add the amount of new testdata to the running server, e.g.:
+python app.py
+python app.py add-testdata 2
+"""
+    if len(sys.argv) == 3:
+        amount = 0
+        hasParsedAmount = False
+        try: 
+            amount = int(sys.argv[2])
+            hasParsedAmount = True
+        except ValueError:
+            hasParsedAmount = False
+        
+        if sys.argv[1] == 'add-testdata' and hasParsedAmount:
+            import urllib2
+            from random import randint
+            from datetime import datetime, timedelta
+            
+            data = []
+            for i in range(0, amount):
+                data.append(generateTestdata())
+            
+            req = urllib2.Request('http://localhost:5000/service/adddownloads')
+            req.add_header('Content-Type', 'application/json')
+
+            response = urllib2.urlopen(req, json.dumps(data))
+            print response
+        else:
+            print usage
+    elif len(sys.argv) == 1:
+        app.run(threaded=True,debug=True,host='0.0.0.0')
+    else:
+        print usage
